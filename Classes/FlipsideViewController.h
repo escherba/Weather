c6c76f1a2231ec6e913d642ef8c702f038dad99a
escherba <escherba@gmail.com>
2011-11-09 13:58:42 -0500

    Initial commit

---
+++ Classes/FlipsideViewController.h
@@ -0,0 +1,26 @@
+//
+//  FlipsideViewController.h
+//  Weather
+//
+//  Created by Eugene Scherba on 1/11/11.
+//  Copyright 2011 Boston University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol FlipsideViewControllerDelegate;
+
+
+@interface FlipsideViewController : UIViewController {
+	id <FlipsideViewControllerDelegate> delegate;
+}
+
+@property (nonatomic, assign) id <FlipsideViewControllerDelegate> delegate;
+- (IBAction)done:(id)sender;
+@end
+
+
+@protocol FlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller;
+@end
+
